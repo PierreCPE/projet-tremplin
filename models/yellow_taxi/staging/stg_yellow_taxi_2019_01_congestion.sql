4162958bd64094973adc6f4cd9ac9bcc63eaec95
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-24 12:46:33 +0000

    changement pour prep la table 2019-01 pour l'union

--- models/yellow_taxi/staging/stg_yellow_taxi_2019_01_congestion.sql
+++ models/yellow_taxi/staging/stg_yellow_taxi_2019_01_congestion.sql
@@ -1,17 +1,10 @@
-with 
-yellow_tripdata_2019_01_congestion_fix
-
-as (
-select
-CAST(IFNULL(congestion_surcharge, '0') AS congestion_surcharge
-
-from 
-{{ source('yellow_taxi', 'yellow_tripdata_2019_01') }}
+WITH yellow_tripdata_2019_01_congestion_fix AS (
+    SELECT,
+        CAST(IFNULL(congestion_surcharge, '0') AS FLOAT64) AS congestion_surcharge
+    FROM 
+        {{ source('yellow_taxi', 'yellow_tripdata_2019_01') }}
 )
 
 SELECT 
-    congestion_surcharge,
-    COUNT(*) AS count
+    *
 FROM yellow_tripdata_2019_01_congestion_fix
-GROUP BY congestion_surcharge
-ORDER BY count DESC

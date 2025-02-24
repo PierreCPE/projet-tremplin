bac47b5d7ddc7bf9cd9927e556733be0e045883d
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-24 13:43:38 +0000

    delete tout les nuls dans la table agrégé

--- models/yellow_taxi/staging/stg_yellow_taxi_agg.sql
+++ models/yellow_taxi/staging/stg_yellow_taxi_agg.sql
@@ -1,112 +1,115 @@
 WITH concatenated_data AS (
     SELECT 
         *,
-        
         CONCAT('2019_01_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ ref('stg_yellow_taxi_2019_01_congestion') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_02_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_02') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_03_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_03') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_04_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_04') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_05_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_05') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_06_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_06') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_07_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_07') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_08_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_08') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_09_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_09') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_10_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_10') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_11_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_11') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2019_12_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_12') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2020_01_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_01') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2020_02_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_02') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2020_03_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_03') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2020_04_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_04') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2020_05_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_05') }}
     UNION ALL
     SELECT 
         *,
-        
         CONCAT('2020_06_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_06') }}
 )
 
-SELECT * 
+SELECT 
+    *
 FROM concatenated_data
+WHERE 
+    trip_id IS NOT NULL AND
+    VendorID IS NOT NULL AND
+    tpep_pickup_datetime IS NOT NULL AND
+    tpep_dropoff_datetime IS NOT NULL AND
+    passenger_count IS NOT NULL AND
+    trip_distance IS NOT NULL AND
+    RatecodeID IS NOT NULL AND
+    store_and_fwd_flag IS NOT NULL AND
+    PULocationID IS NOT NULL AND
+    DOLocationID IS NOT NULL AND
+    payment_type IS NOT NULL AND
+    fare_amount IS NOT NULL AND
+    extra IS NOT NULL AND
+    mta_tax IS NOT NULL AND
+    tip_amount IS NOT NULL AND
+    tolls_amount IS NOT NULL AND
+    improvement_surcharge IS NOT NULL AND
+    total_amount IS NOT NULL AND
+    congestion_surcharge IS NOT NULL
--- models/yellow_taxi/staging/stg_yellow_taxi_agg_check_null.sql
+++ models/yellow_taxi/staging/stg_yellow_taxi_agg_check_null.sql
@@ -19,3 +19,71 @@ SELECT
     SUM(CASE WHEN total_amount IS NULL THEN 1 ELSE 0 END) AS total_amount_null_count,
     SUM(CASE WHEN congestion_surcharge IS NULL THEN 1 ELSE 0 END) AS congestion_surcharge_null_count
 FROM {{ ref('stg_yellow_taxi_agg') }}
+
+-- Pour la premiere itération que l'on a eu :
+-- On a environ 500 000 valeures nulles sur 100 000 000 ce qui est négligeable si elles sont toutes sur les meme lignes (donc 500 000 lignes en moins) on va vérif : 
+-- Pour la deuxieme ( un fois le code de dessous executé) nous avons bien 0 valeurs nulles en tout dans la table aggrégé
+
+
+
+-- SELECT
+--     trip_id,
+--     VendorID,
+--     tpep_pickup_datetime,
+--     tpep_dropoff_datetime,
+--     passenger_count,
+--     trip_distance,
+--     RatecodeID,
+--     store_and_fwd_flag,
+--     PULocationID,
+--     DOLocationID,
+--     payment_type,
+--     fare_amount,
+--     extra,
+--     mta_tax,
+--     tip_amount,
+--     tolls_amount,
+--     improvement_surcharge,
+--     total_amount,
+--     congestion_surcharge,
+--     (CASE WHEN VendorID IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN tpep_pickup_datetime IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN tpep_dropoff_datetime IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN passenger_count IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN trip_distance IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN RatecodeID IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN store_and_fwd_flag IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN PULocationID IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN DOLocationID IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN payment_type IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN fare_amount IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN extra IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN mta_tax IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN tip_amount IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN tolls_amount IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN improvement_surcharge IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN total_amount IS NULL THEN 1 ELSE 0 END +
+--      CASE WHEN congestion_surcharge IS NULL THEN 1 ELSE 0 END) AS null_count
+-- FROM {{ ref('stg_yellow_taxi_agg') }}
+-- WHERE (VendorID IS NULL OR
+--        tpep_pickup_datetime IS NULL OR
+--        tpep_dropoff_datetime IS NULL OR
+--        passenger_count IS NULL OR
+--        trip_distance IS NULL OR
+--        RatecodeID IS NULL OR
+--        store_and_fwd_flag IS NULL OR
+--        PULocationID IS NULL OR
+--        DOLocationID IS NULL OR
+--        payment_type IS NULL OR
+--        fare_amount IS NULL OR
+--        extra IS NULL OR
+--        mta_tax IS NULL OR
+--        tip_amount IS NULL OR
+--        tolls_amount IS NULL OR
+--        improvement_surcharge IS NULL OR
+--        total_amount IS NULL OR
+--        congestion_surcharge IS NULL)
+-- ORDER BY null_count DESC
+
+
+-- On se rend compte que les valeures nulles sont sur la meme ligne donc on va les enlever lors de la concaténation des tables sources

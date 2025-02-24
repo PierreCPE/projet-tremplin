3b8c7dea4ba410e991586c37f9a04f391a94e5bf
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-24 15:17:20 +0000

    Refactoring pour le gold du payment_summary

--- models/yellow_taxi/marts/payment/fct_yellow_taxi_payment_trends.sql
+++ models/yellow_taxi/marts/payment/fct_yellow_taxi_payment_trends.sql
@@ -8,4 +8,4 @@ SELECT
 FROM {{ ref('stg_yellow_taxi_payment_trends') }} AS ad
 JOIN {{ ref('stg_yellow_taxi_payment_type') }} AS pt
 ON ad.payment_type = pt.payment_type
-ORDER BY ad.month, ad.day_of_week, ad.trip_count DESC
+-- ORDER BY ad.month, ad.day_of_week, ad.trip_count DESC

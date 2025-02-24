1f3b256302d7d53048f7d6f79864c72d92a11006
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-24 12:28:05 +0000

    refactoring du staging

--- models/yellow_taxi/staging/stg_yellow_taxi_agg.sql
+++ models/yellow_taxi/staging/stg_yellow_taxi_agg.sql
@@ -1,95 +1,112 @@
 WITH concatenated_data AS (
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_01_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_01') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_02_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_02') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_03_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_03') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_04_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_04') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_05_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_05') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_06_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_06') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_07_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_07') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_08_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_08') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_09_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_09') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_10_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_10') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_11_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_11') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2019_12_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2019_12') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2020_01_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_01') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2020_02_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_02') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2020_03_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_03') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2020_04_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_04') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2020_05_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_05') }}
     UNION ALL
     SELECT 
         *,
+        CAST(congestion_surcharge AS FLOAT64) AS congestion_surcharge,
         CONCAT('2020_06_', CAST(ROW_NUMBER() OVER() AS STRING)) AS trip_id
     FROM {{ source('yellow_taxi', 'yellow_tripdata_2020_06') }}
 )
 
 SELECT * 
 FROM concatenated_data
-

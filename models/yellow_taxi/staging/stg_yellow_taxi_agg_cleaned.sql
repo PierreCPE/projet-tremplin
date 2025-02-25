20b153040ea7caf5389f6f65731a14fb089aa03a
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-25 12:28:16 +0000

    Ajout de filtre pour al agg_cleaned

--- models/yellow_taxi/staging/stg_yellow_taxi_agg_cleaned.sql
+++ models/yellow_taxi/staging/stg_yellow_taxi_agg_cleaned.sql
@@ -1,3 +1,4 @@
+-- On veut clean la date car les mois ne sont pas bons 
 WITH filtered_data AS (
     SELECT 
         *
@@ -5,6 +6,15 @@ WITH filtered_data AS (
     WHERE 
         DATE_TRUNC(tpep_pickup_datetime, MONTH) BETWEEN '2019-01-01' AND '2020-06-30'
         AND DATE_TRUNC(tpep_dropoff_datetime, MONTH) BETWEEN '2019-01-01' AND '2020-06-30'
+        AND trip_distance >= 0 AND trip_distance <= 80 AND 
+        fare_amount >= 0 AND fare_amount <= 10000 AND
+        extra >= 0 AND extra <= 1.5 AND
+        tip_amount >= 0 AND tip_amount <= 10000 AND
+        tolls_amount >= 0 AND tolls_amount <= 5000 AND
+        total_amount >= 0 AND total_amount <= 15000 
+        AND
+        improvement_surcharge = 0.3
+
 )
 
 SELECT 

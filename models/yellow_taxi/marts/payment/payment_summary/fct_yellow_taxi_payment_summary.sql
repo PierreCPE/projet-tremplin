7958635c843ffc1b9e0cdfcac6fcfbb408e994f8
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-26 09:59:16 +0000

    Changmt de logique pour le gold summary, car la comparaison etait faite en prenant en compte les tips des cartes alors que cash n'en a pas

--- models/yellow_taxi/marts/payment/payment_summary/fct_yellow_taxi_payment_summary.sql
+++ models/yellow_taxi/marts/payment/payment_summary/fct_yellow_taxi_payment_summary.sql
@@ -1,12 +1,13 @@
 {% set payment_type_name = get_payment_type_name('payment_type') %}
 
-
 SELECT 
   payment_type, 
   {{ payment_type_name }} AS payment_type_name,
   COUNT(*) AS total_trips, 
-  SUM(total_amount) AS total_revenue, 
-  AVG(total_amount) AS avg_fare
+  SUM(total_amount + CASE WHEN payment_type = 1 THEN tip_amount ELSE 0 END) AS total_revenue_with_tips,
+  SUM(total_amount) AS total_revenue_without_tips,
+  AVG(total_amount + CASE WHEN payment_type = 1 THEN tip_amount ELSE 0 END) AS avg_fare_with_tips,
+  AVG(total_amount) AS avg_fare_without_tips
 FROM {{ ref('stg_yellow_taxi_agg_cleaned') }}
 GROUP BY payment_type
-ORDER BY total_revenue DESC
+ORDER BY total_revenue_with_tips DESC

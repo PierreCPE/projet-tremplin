64dc8ab382b78f631129f8d63848e89ef073be89
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-26 10:07:22 +0000

    modifs du gold le calcul du montant avec et sans tips n'etait pas bon

--- models/yellow_taxi/marts/payment/payment_summary/fct_yellow_taxi_payment_summary.sql
+++ models/yellow_taxi/marts/payment/payment_summary/fct_yellow_taxi_payment_summary.sql
@@ -4,10 +4,10 @@ SELECT
   payment_type, 
   {{ payment_type_name }} AS payment_type_name,
   COUNT(*) AS total_trips, 
-  SUM(total_amount + CASE WHEN payment_type = 1 THEN tip_amount ELSE 0 END) AS total_revenue_with_tips,
-  SUM(total_amount) AS total_revenue_without_tips,
-  AVG(total_amount + CASE WHEN payment_type = 1 THEN tip_amount ELSE 0 END) AS avg_fare_with_tips,
-  AVG(total_amount) AS avg_fare_without_tips
+  SUM(total_amount) AS total_revenue_with_tips,
+  SUM(total_amount - tip_amount) AS total_revenue_without_tips,
+  AVG(total_amount) AS avg_fare_with_tips,
+  AVG(total_amount - tip_amount) AS avg_fare_without_tips
 FROM {{ ref('stg_yellow_taxi_agg_cleaned') }}
 GROUP BY payment_type
 ORDER BY total_revenue_with_tips DESC

25e3e3f82c66f8642b3011429427f880d7ae0fcb
Pierre Gosson <pierre.gosson@keyrus.com>
2025-02-28 08:23:10 +0000

    refacto et changment de dim location

--- models/yellow_taxi/marts/payment/payment_location/dim_yellow_taxi_location.sql
+++ models/yellow_taxi/marts/payment/payment_location/dim_yellow_taxi_location.sql
@@ -1,7 +1,18 @@
-SELECT
-LocationID,
-Borough,
-Zone,
-service_zone
+WITH stg_temp_location AS (
+    SELECT
+        LocationID,
+        Borough,
+        Zone,
+        service_zone
+    FROM 
+        {{ ref('stg_yellow_taxi_location') }}
+)
+
+SELECT 
+    fct.PULocationID,
+    stg_temp_location.*
 FROM 
-{{ ref('stg_yellow_taxi_location') }}
+    {{ ref('fct_yellow_taxi_payment_location') }} AS fct
+LEFT JOIN 
+    stg_temp_location
+    ON fct.PULocationID = stg_temp_location.LocationID
